5b424faea58413adf1b3d4252d584a4916591b31
SHERAWAT LOKESH <107927380+sherawat-Lokesh@users.noreply.github.com>
2024-10-11 03:55:16 +0530

    fixed error or buffering on load

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Spline from '@splinetool/react-spline';
+import React, { Suspense } from 'react';
+const Spline = React.lazy(() => import('@splinetool/react-spline'));
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Spline style={{ height: '100vh' }} className='w-screen' scene="https://prod.spline.design/Ic7C21XebvCNdNjo/scene.splinecode" />
+    
+    <Suspense fallback={<div>Loading...</div>}>
+        <Spline style={{ height: '100vh' }} className='w-screen' scene="https://prod.spline.design/Ic7C21XebvCNdNjo/scene.splinecode" />
+      </Suspense>
   </StrictMode>,
 )
